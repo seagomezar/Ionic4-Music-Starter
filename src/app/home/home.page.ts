94b4549f330f7e209d7362646b99593674066119
Sebastian Alonso Gomez Arias <seagomezar@gmail.com>
2019-08-22 18:47:28 -0500

    Removing slides content to a better place. In this step we put the logic in the controller and we used ngFor to create slides dinamically

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -12,5 +12,34 @@ export class HomePage {
     centeredSlides: true,
     speed: 400
   };
+  slides = [
+    {
+      imageSrc: "assets/img/logo.png",
+      imageAlt: "Platzi Music Logo",
+      title: "Escucha tu música",
+      subTitle: "EN CUALQUIER LUGAR",
+      description: `Los mejores álbumes, las mejores canciones. Escucha y comparte en
+        cualquier momento, a todas horas.`,
+      icon: "play"
+    },
+    {
+      imageSrc: "assets/img/logo.png",
+      imageAlt: "Platzi Music Logo",
+      title: "Disfruta de nuestro reproductor",
+      subTitle: "DE VIDEOS INCREÍBLES",
+      description: `Entra al modo video de nuestro reproductor y obtén acceso a clips,
+        documentales y making offs incríbles de tu artista favorito.`,
+      icon: "videocam"
+    },
+    {
+      imageSrc: "assets/img/logo.png",
+      imageAlt: "Platzi Music Logo",
+      title: "Accede al exclusivo",
+      subTitle: "MODO DEPORTE",
+      description: `Crea una playlist basada en tu actividad física. <br />
+        Ten reportes y acceso a lo que necesites, integrado ccon GPS!`,
+      icon: "bicycle"
+    }
+  ];
   constructor() {}
 }
